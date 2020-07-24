85ad218a90567830e9d2395a652188825edfe1cd
bpbeernie <bpbeernie@gmail.com>
2020-07-24 15:57:27 -0700

    fix migration

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MySql.Data.MySqlClient;
+using System;
+using System.Threading;
 using TaskAPI.Models;
 using TasksWebService.Models;
 
@@ -27,20 +30,49 @@ namespace TaskAPI
             var user = Configuration["DBUser"];
             var password = Configuration["DBPassword"];
             var database = Configuration["Database"];
+            var connectionString = $"server={server};Port={port};Database={database};Uid={user};Pwd={password};";
+
+            WaitForDbInit(connectionString);
 
             services.AddDbContext<TaskContext>(options => 
             options.UseMySql(
-                $"Server={server};Port={port};Database={database};User ID={user};Password={password};"));
+                connectionString, builder =>
+                {
+                    builder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
+                }));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
-        
+
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseMvc();
 
             PrepDB.PrepPopulation(app);
+        }
+
+        private static void WaitForDbInit(string connectionString)
+        {
+            var connection = new MySqlConnection(connectionString);
+            Console.WriteLine("Connecting with credentials: {0}", connectionString);
+
+            var retries = 1;
+            while (retries < 5)
+            {
+                try
+                {
+                    Console.WriteLine("Connecting to db. Trial: {0}", retries);
+                    connection.Open();
+                    connection.Close();
+                    break;
+                }
+                catch (MySqlException)
+                {
+                    Thread.Sleep((int)Math.Pow(2, retries) * 1000);
+                    retries++;
+                }
+            }
         }
     }
 }
